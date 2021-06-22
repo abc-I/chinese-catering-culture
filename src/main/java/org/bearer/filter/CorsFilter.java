4b44f883009afc6c2d608c1e6455a8bc25aa29ef
abc-I <1773530070@qq.com>
2021-06-22 20:13:19 +0800

    abc-I Update CorsFilter

--- src/main/java/org/bearer/filter/CorsFilter.java
+++ src/main/java/org/bearer/filter/CorsFilter.java
@@ -1,8 +1,10 @@
 package org.bearer.filter;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -10,29 +12,38 @@ import java.io.IOException;
 /**
  * @author Li
  * @version 1.0
- * @date Created in 2021/6/22 18:19
+ * @date Created in 2021/4/11 10:57
  */
-@Component
+@WebFilter("/*")
 public class CorsFilter implements Filter {
-
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Credentials", "false");
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type,JwtToken");
-        if ("OPTIONS".equals(((HttpServletRequest) request).getMethod())) {
-            response.getWriter().println("ok");
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 设置请求编码，解决中文乱码
+        request.setCharacterEncoding("UTF-8");
+
+        // 设置响应编码，解决中文乱码
+        response.setContentType("test/html;charset=UTF-8");
+        //Filter过滤器跨域处理
+        //设置请求方式为http 的rest 风格
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        // 允许客户端携带跨域cookie
+        // 当Access-Control-Allow-Credentials设为true的时候，Access-Control-Allow-Origin不能设为星号
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        // 允许指定域访问跨域资源
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        // 允许浏览器发送的请求消息头
+        response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        // 允许浏览器在预检请求成功之后发送的实际请求方法名
+        response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
+        // 设置响应数据格式
+        response.setHeader("Content-Type", "application/json");
+        // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
             return;
         }
-        chain.doFilter(request, response);
-    }
-    @Override
-    public void destroy() {
-    }
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+        filterChain.doFilter(request, response);
     }
 }
